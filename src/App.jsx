e602d2023fb3ed8c5c3f6570076e8e913a9cdaee
gsh0581 <5033924549@qq.com>
2019-10-30 20:54:56 +0800

    使用静态属性contextType 直接获取值

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import React,{createContext,useState} from 'react';
+import React,{Component,createContext,useState} from 'react';
 import './App.css';
 const BatteryContext = createContext()
-const OnlineContext = createContext()
-const Leaf =()=>{
-  return (
-    <BatteryContext.Consumer>
-    {
-      battery => (
-        <OnlineContext.Consumer>
-          {
-            online => <h1>battery ,{battery},online,{String(online)}</h1>
-          }
-        </OnlineContext.Consumer>
-      )
-    }
-    </BatteryContext.Consumer>
-  )
+
+class Leaf extends Component {
+  static contextType = BatteryContext
+  render(){
+    const battery = this.context
+    return (<h1>{battery}</h1>)
+    
+  }
 }
 const Middle  = ()=>{
   return (
@@ -24,15 +17,12 @@ const Middle  = ()=>{
 }
 function App() {
   const [battery, setBattery] = useState(60);
-  const [online, setOnline] = useState(false);
   return (
     <div className="App">
       <BatteryContext.Provider value={battery}>
-      <OnlineContext.Provider value={online}>
       <button type="button" onClick={()=>setBattery(battery-1)}>Press</button>
-      <button type="button" onClick={()=>setOnline(!online)}>Press</button>
         <Middle />
-      </OnlineContext.Provider>
+      
       </BatteryContext.Provider>
     </div>
   );
