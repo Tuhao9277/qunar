52536cad652070683a3466da6e1c2a5910a13ce4
gsh0581 <5033924549@qq.com>
2020-02-24 00:30:53 +0800

    添加车次点击跳转

--- src/query/List.jsx
+++ src/query/List.jsx
@@ -1,7 +1,8 @@
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 import './List.css'
-
+import { useMemo } from 'react'
+import URI from 'urijs'
 const ListItem = memo(
   ({
     dTime,
@@ -9,43 +10,54 @@ const ListItem = memo(
     dStation,
     aStation,
     trainNumber,
+    date,
     time,
     priceMsg,
     dayAfter,
-  }) => (
-    <li className="list-item">
-      <a href="">
-        <span className="item-time">
-          <em>{dTime}</em>
-          <br />
-          <em className="em-light">
-            {aTime} <i className="time-after">{dayAfter}</i>{' '}
-          </em>
-        </span>
-        <span className="item-stations">
-          <em className="em-light">
-            <i className="train-station train-start">始</i>
-            {dStation}
-          </em>
-          <br/>
-          <em className="em-light">
-            <i className="train-station train-end">终</i>
-            {aStation}
-          </em>
-        </span>
-        <span className="item-train">
-        <em>{trainNumber}</em>
-        <br/>
-        <em className="em-light">{time}</em>
-        </span>
-        <span className="item-ticket">
-        <em>{priceMsg}</em>
-        <br/>
-        <em className="em-light-orange">可抢票</em>
-        </span>
-      </a>
-    </li>
-  ),
+  }) => {
+    const url = useMemo(() => {
+      return new URI('ticket.html')
+        .setSearch('dStation', dStation)
+        .setSearch('aStation', aStation)
+        .setSearch('trainNumber', trainNumber)
+        .setSearch('date', date)
+        .toString()
+    }, [aStation, dStation, trainNumber, date])
+    return (
+      <li className="list-item">
+        <a href={url}>
+          <span className="item-time">
+            <em>{dTime}</em>
+            <br />
+            <em className="em-light">
+              {aTime} <i className="time-after">{dayAfter}</i>{' '}
+            </em>
+          </span>
+          <span className="item-stations">
+            <em className="em-light">
+              <i className="train-station train-start">始</i>
+              {dStation}
+            </em>
+            <br />
+            <em className="em-light">
+              <i className="train-station train-end">终</i>
+              {aStation}
+            </em>
+          </span>
+          <span className="item-train">
+            <em>{trainNumber}</em>
+            <br />
+            <em className="em-light">{time}</em>
+          </span>
+          <span className="item-ticket">
+            <em>{priceMsg}</em>
+            <br />
+            <em className="em-light-orange">可抢票</em>
+          </span>
+        </a>
+      </li>
+    )
+  },
 )
 const List = memo(({ list }) => {
   return (
